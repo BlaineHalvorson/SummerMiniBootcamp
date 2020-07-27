7ef1b1015c26666d2621c8b447139659577b1e3e
BlaineHalvorson <blainehalvorson29@gmail.com>
2020-07-27 17:42:36 -0400

    class 7/27: Started working with Angular

---
+++ project-frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ProjectListComponent } from './components/project-list/project-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProjectListComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
