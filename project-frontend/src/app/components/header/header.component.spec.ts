ff112d94407334ebafad0d8a49a697d491f3f867
Blaine Halvorson <blainehalvorson@Blaines-MacBook-Pro.local>
2020-08-04 00:10:24 -0400

    Class 8/3

--- project-frontend/src/app/components/header/header.component.spec.ts
+++ project-frontend/src/app/components/header/header.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProjectListComponent } from './project-list.component';
+import { HeaderComponent } from './header.component';
 
-describe('ProjectListComponent', () => {
-  let component: ProjectListComponent;
-  let fixture: ComponentFixture<ProjectListComponent>;
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProjectListComponent ]
+      declarations: [ HeaderComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ProjectListComponent);
+    fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- project-frontend/src/app/components/services/project.service.ts
+++
@@ -1,4 +0,0 @@
-deleteTask(task: Task): void{
-    this.http.delete(API_URL + "/tasks/" + TextTrackList.id)
-        .subscribe((response:any) => this.getProjects());
-}
